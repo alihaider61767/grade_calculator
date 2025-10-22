7806d50af83817609eb1426c9ab000eda6288834
Ali Haider <alihaider61767@gmail.com>
2025-10-21 23:19:35 -0700

    update py

--- grade_calculator.py
+++ grade_calculator.py
@@ -2,3 +2,6 @@ print("=== Student Grade Calculator ===")
 print("")
 
 student_name = input("Enter stdent name: ")
+
+print("Enter marks out of 100 in each subject")
+english = int(input("English marks: "))
