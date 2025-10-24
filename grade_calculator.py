a35b02a4f144eb7ca5d79c3ac33537cb7116b213
Ali Haider <alihaider61767@gmail.com>
2025-10-23 23:09:37 -0700

    add new update

--- grade_calculator.py
+++ grade_calculator.py
@@ -7,3 +7,10 @@ print("Enter marks out of 100 in each subject")
 english = int(input("English marks: "))
 math = int(input("Math marks: "))
 science = int(input("Science marks: "))
+
+total = english + math + science 
+percentage = total / 3
+
+print("")
+print("--- Result ---")
+print("Student Name:" , student_name)
